f03cde6c986c2fdcc549c1158543fda473f205a2
Julia Bielecka <juliabiel25@gmail.com>
2024-08-05 22:38:02 +0200

    update scene camera setting constants

--- constants.js
+++ constants.js
@@ -1,7 +1,12 @@
 export const FOLDER_PATH = "../../models/_GLTF/";
-export const FILENAME = "flowering.gltf";
+export const FILENAME = "teapot-og.gltf";
 
 // for manually supressing fps to run script in V-sync disabled environment
 // without overloading cpu
 export const FPS_LIMIT = 500;
 export const FPS_INTERVAL = 1000 / FPS_LIMIT;
+
+
+ export const CAMERA_RADIUS = 7; 
+ export const ORIGIN_HEIGHT = 5; 
+ export const CAMERA_HEIGHT = 3;
